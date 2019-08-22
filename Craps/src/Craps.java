ab4cbdb0fb458c6c48b24718bcd774b034792279
jonathan.egan <jonathan.egan@MU-C1-12.mullen.pvt.k12.co.us>
2019-08-22 11:36:14 -0600

    commit

---
+++ Craps/src/Craps.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Craps 
+{
+	public static void main(String[] args) 
+	{
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = userInput.nextLine();
+		System.out.println("Hi " + name);
+		
+		System.out.println("");
+		
+		
+	}
+}
